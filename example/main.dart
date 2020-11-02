e2c34e90355bcc0ac8397c3c9b5616f2951a9cb4
nvrr <sureshvarma809@gmail.com>
2020-11-02 11:55:45 +0530

    2 -import statements and make use of Stdout instance and Testing Import class

---
+++ example/main.dart
@@ -0,0 +1,7 @@
+import 'package:prompter_nvr/src/terminal.dart';
+
+void main() {
+  var terminal = Terminal();
+
+  terminal.printPrompt('Hi there!');
+}
---
+++ lib/src/terminal.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+class Terminal {
+  void printPrompt(String prompt) {
+    stdout.writeln(prompt);
+  }
+
+  void clearScreen() {
+    if (Platform.isWindows) {
+
+    }else {
+
+    }
+  }
+  
+}
